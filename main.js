d94c967bd207f33552d4d1e149802c715f0bed5b
hinanadir123 <162301677+hinanadir123@users.noreply.github.com>
2024-03-31 15:45:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+//  import { log } from "console";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+//  let members: string[] =[ 'rida' , 'aisal', 'zobia', 'sana', 'mahjabben', 'kaosr'];
+//  for(let i=0;i<members.length; i++){
+//      console.log(members[i]);
+//  }
+// // for each
+// members.forEach(name => {
+//     console.log(name);
+// });
+// question 12
+// let members: string[] =[ 'rida' , 'aisal', 'zobia', 'sana', 'mahjabben', 'kaosr'];
+// let message: string= "who is the most intelligent girl:"?
+// for(let i=0;i<members.length; i++){
+//     console.log(message + members[i]);
+// }
+// // question 13
+// let transportation: string [] = ['land cruiser', 'civic', 'jeep', 'mercedees'];
+// for(let i=0;i<transportation.length; i++){
+//     console.log( 'i would like to own a ' + transportation[i]);
+// }
+// task 14 question
+var guestlist = ['rids', 'kasaf', 'hina', 'aisal'];
+for (let i = 0; i < guestlist.length; i++) {
+   console.log('respected sir/bro' + guestlist[i] + ',\nplz do not forget attendig the party.\n\nThank you');
+ }
+export{guestlist}
+
+
+
+
+// //task 15
+var notPresent = "hina";
+var new_guest = "sana";
+guestlist[2] = new_guest;
+//  for (let i = 0; i < guestlist.length; i++){
+//    console.log('respected sir/bro' + guestlist[i] + ',\nplz do not forget attendig the party.\nThank you');
+//  }
+// //     task 16
+// guestlist.unshift('swera', 'kasira', 'zainab', 'lareb');
+// //  for (let i = 0; i < guestlist.length; i++){
+// //    console.log('respected sir/bro' + guestlist[i] + ',\nplz do not forget attendig the party.\nThank you');
+// //  }
+// // task 18
+// var places = ['karaci', 'lahore', 'mps', 'iraq', 'asia'];
+// //console.log('origional'+ places);
+// //console.log("copy " +  [...places]. sort());
+// //show that your array is still in its origional order by printing it.
+// //console.log('origional'+ places);
+// // print array in reverse
+// console.log("copy " + __spreadArray([], places, true).sort().reverse());
+// //show your array is still in its origionnal order
+// console.log("copy " + __spreadArray([], places, true).sort().reverse());
+// //print te array to show that its order has changed.
+// console.log('origional' + places.sort);
+// console.log('origional' + places.sort().reverse());
+
+
+
+
+
+
+
+   //task 19
---
+++ main.ts
@@ -0,0 +1,88 @@
+//  import { log } from "console";
+
+//  let members: string[] =[ 'rida' , 'aisal', 'zobia', 'sana', 'mahjabben', 'kaosr'];
+
+//  for(let i=0;i<members.length; i++){
+//      console.log(members[i]);
+//  }
+
+
+// // for each
+// members.forEach(name => {
+//     console.log(name);
+// });
+
+
+
+// question 12
+
+
+// let members: string[] =[ 'rida' , 'aisal', 'zobia', 'sana', 'mahjabben', 'kaosr'];
+// let message: string= "who is the most intelligent girl:"?
+// for(let i=0;i<members.length; i++){
+//     console.log(message + members[i]);
+// }
+
+
+// // question 13
+// let transportation: string [] = ['land cruiser', 'civic', 'jeep', 'mercedees'];
+
+// for(let i=0;i<transportation.length; i++){
+//     console.log( 'i would like to own a ' + transportation[i]);
+// }
+
+
+
+// task 14 question
+ let guestlist = ['rids', 'kasaf', 'hina', 'aisal'];
+//for (let i = 0; i < guestlist.length; i++) {
+  //   console.log('respected sir/bro' + guestlist[i] + ',\nplz do not forget attendig the party.\n\nThank you');
+// }
+
+
+// //task 15
+ let notPresent: string= "hina";
+ let new_guest: string = "sana";
+ guestlist[2] = new_guest;
+//  for (let i = 0; i < guestlist.length; i++){
+//    console.log('respected sir/bro' + guestlist[i] + ',\nplz do not forget attendig the party.\nThank you');
+//  }
+
+
+
+//     task 16
+
+ guestlist.unshift('swera', 'kasira', 'zainab', 'lareb');
+//  for (let i = 0; i < guestlist.length; i++){
+//    console.log('respected sir/bro' + guestlist[i] + ',\nplz do not forget attendig the party.\nThank you');
+//  }
+
+
+
+
+// task 18
+
+let places: string[] = ['karaci', 'lahore', 'mps', 'iraq', 'asia' ];
+
+//console.log('origional'+ places);
+
+//console.log("copy " +  [...places]. sort());
+
+//show that your array is still in its origional order by printing it.
+
+//console.log('origional'+ places);
+
+// print array in reverse
+
+console.log("copy " +  [...places].sort() .reverse());
+
+//show your array is still in its origionnal order
+
+console.log("copy " +  [...places].sort() .reverse());
+
+//print te array to show that its order has changed.
+
+console.log('origional'+ places.sort);
+ 
+console.log('origional'+ places.sort().reverse());
+
